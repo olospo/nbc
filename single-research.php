2de6474e81cfdea84e17494e5d96a38844019d3b
Tom Brooks <tom@olospo.co.uk>
2024-05-07 10:46:03 +0100

    Reseearch updates

--- single-research.php
+++ single-research.php
@@ -5,13 +5,17 @@ while ( have_posts() ) : the_post(); ?>
   
 <section class="research hero">
   <div class="background" style="background: url(' <?php bloginfo('template_directory'); ?>/img/research.jpg') center center no-repeat; background-size: cover;"></div>
+  <?php
+  $icon = get_field('icon');
+  if( !empty( $icon ) ):  ?>
   <div class="container">
     <div class="eight columns">
       <div class="circle">
-        <?php if ( has_post_thumbnail() ) : the_post_thumbnail(); endif; ?>
+        <img src="<?php echo esc_url($icon['url']); ?>" alt="<?php echo esc_attr($icon['alt']); ?>" />
       </div>
     </div>
   </div>
+  <?php endif; ?>
 </section>
 
 <section class="post research">
@@ -29,7 +33,16 @@ while ( have_posts() ) : the_post(); ?>
       <aside class="three columns">
         <div class="team_lead">
           <h3>Team Lead</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <?php
+          $team_lead = get_field('team_lead');
+          if ($team_lead): ?>
+            <?php if (has_post_thumbnail($team_lead->ID)): ?>
+            <div class="team-lead-thumbnail">
+              <?php echo get_the_post_thumbnail($team_lead->ID, 'large-thumb'); ?>
+            </div>
+            <?php endif; ?>
+            <h4><?php echo esc_html($team_lead->post_title); ?></h4>
+          <?php endif; ?>
         </div>
       </aside>
     </div>
