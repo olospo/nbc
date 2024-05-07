0c05d72250beb69810ab36c3b9633c99c2f2c280
Tom Brooks <tom@olospo.co.uk>
2024-05-07 13:33:37 +0100

    Research page

--- single-research.php
+++ single-research.php
@@ -31,23 +31,73 @@ while ( have_posts() ) : the_post(); ?>
     <div class="row">
       <div class="nine columns">
         <h1><?php the_title(); ?></h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="nine columns">
         <?php the_content(); ?>
       </div>
       <aside class="three columns">
-        <div class="team_lead">
+        <div class="team-lead">
           <h3>Team Lead</h3>
           <?php
           $team_lead = get_field('team_lead');
           if ($team_lead): ?>
             <?php if (has_post_thumbnail($team_lead->ID)): ?>
-            <div class="team-lead-thumbnail">
-              <?php echo get_the_post_thumbnail($team_lead->ID, 'large-thumb'); ?>
-            </div>
+              <div class="team-lead-thumbnail">
+                <?php echo get_the_post_thumbnail($team_lead->ID, 'large-thumb'); ?>
+                <h4><?php echo esc_html($team_lead->post_title); ?></h4>
+                <p>Job Title</p>
+              </div>
+              <?php else: ?>
+              <div class="team-lead-thumbnail">
+                <img src="<?php bloginfo('template_directory'); ?>/img/default-team.png" alt="<?php echo esc_html($team_lead->post_title); ?>">
+                <h4><?php echo esc_html($team_lead->post_title); ?></h4>
+                <p>Job Title</p>
+              </div>
             <?php endif; ?>
-            <h4><?php echo esc_html($team_lead->post_title); ?></h4>
           <?php endif; ?>
         </div>
+        <div class="research-buttons">
+          <a href="#" class="button filled black">Publications</a>
+          <a href="#" class="button filled">Projects</a>
+        </div>
       </aside>
+    </div>
+  </div>
+</section>
+
+<section class="research-team">
+  <div class="container">
+    <div class="row">
+      <h3>Research Programme Team</h3>
+    </div>
+    <div class="team">
+      <div class="team-thumbnail">
+        <img src="<?php bloginfo('template_directory'); ?>/img/default-team.png">
+        <h4>Person Name</h4>
+        <p>Job Title</p>
+      </div>
+      <div class="team-thumbnail">
+        <img src="<?php bloginfo('template_directory'); ?>/img/default-team.png">
+        <h4>Person Name</h4>
+        <p>Job Title</p>
+      </div>
+      <div class="team-thumbnail">
+        <img src="<?php bloginfo('template_directory'); ?>/img/default-team.png">
+        <h4>Person Name</h4>
+        <p>Job Title</p>
+      </div>
+      <div class="team-thumbnail">
+        <img src="<?php bloginfo('template_directory'); ?>/img/default-team.png">
+        <h4>Person Name</h4>
+        <p>Job Title</p>
+      </div>
+      <div class="team-thumbnail">
+        <img src="<?php bloginfo('template_directory'); ?>/img/default-team.png">
+        <h4>Person Name</h4>
+        <p>Job Title</p>
+      </div>
     </div>
   </div>
 </section>
