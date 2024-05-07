0f3a7518454ca55189e224660d61e97857bb2e4e
Tom Brooks <tom@olospo.co.uk>
2024-05-07 11:11:24 +0100

    Logo switch

--- inc/logo.php
+++ inc/logo.php
@@ -1,3 +1,9 @@
 <a href="<?php echo get_site_url(); ?>">
-  <img src="<?php bloginfo('template_directory'); ?>/img/logo.png" alt="<?php echo bloginfo( 'name' ); ?>">
+  <?php
+  // Check if the current page is the home page
+  if (is_front_page()) { ?>
+    <img src="<?php bloginfo('template_directory'); ?>/img/logo.png" alt="<?php echo bloginfo( 'name' ); ?>">
+  <?php } else { ?>
+    <img src="<?php bloginfo('template_directory'); ?>/img/logo-white.png" alt="<?php echo bloginfo( 'name' ); ?>">
+  <?php } ?>
 </a>
